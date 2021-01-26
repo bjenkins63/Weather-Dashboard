c0d64a16e28508f77cbda91abf328b622cc6c353
Brian Jenkins <studiojenkins@yahoo.com>
2021-01-26 13:43:11 -0500

    first upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#getWeatherForcast").click(function() {
+    $("#getWeatherForecast").click(function() {
         var city = $("#city").val();
         var key = 'ba2a06e48d754582fb2a293987aa4f29';
         $.ajax({
@@ -12,12 +12,44 @@ $(document).ready(function() {
                 var wf = '';
                 $.each(data.weather, function(index, val) {
                     wf += '<p><b>' + data.name + "</b><img src=" + val.icon + ".png></p>" +
-                    data.main.temp + '&deg;F ' + ' | ' + val.main + ", " + val.description
+                    data.main.temp + '&deg;F ' + ' | ' + val.main + ", " + val.description + ' | ' +
+                    ' Humidy: '  + data.main.humidity+ '%' + ' | ' + ' Wind Speed: ' + data.wind.speed + ' mph' 
+                    // + 'UV Index: ' + data.main.
                 });
-                $("#showWeatherForcast").html(wf);
+                $("#showWeatherForecast").html(wf);
             }
-
         })
     })
-
 });
+
+
+
+$("#getFiveForecast").click(function() {
+    var city = $("#city").val();
+        var key = 'ba2a06e48d754582fb2a293987aa4f29'; 
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/forecast',
+        dataType: 'json',
+        type: 'GET',
+        data: {q: city, appid: key, units: 'imperial'},
+
+        success: function(data5){
+            var wf5 = '';
+            $.forEach(data.weather, function(index, val) {
+                wf5 += '<p><b>' + data.name + "</b><img src=" + val.icon + ".png></p>" +
+                data.main.temp + '&deg;F ' + ' | ' + val.main + ", " + val.description + ' | ' +
+                ' Humidy: '  + data.main.humidity+ '%' + ' | ' + ' Wind Speed: ' + data.wind.speed + ' mph' 
+            });
+
+
+
+            $("#showFiveForecast").html(wf5);
+        }
+
+    })
+     
+});
+
+
+
+// api.openweathermap.org/data/2.5/weather?id=524901&appid=YOUR_API_KEY
